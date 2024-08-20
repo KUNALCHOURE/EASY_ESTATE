20e397362df1d9fe6f69677f59edb21f0fe1f890
KUNAL CHOURE <chourekunal4@gmail.com>
2024-08-20 19:36:33 +0530

    updated

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const expressLayouts = require("express-ejs-layouts");
+
+const port = 3030;
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set("layout", "layouts/boilerplate");
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+    res.send("hello");
+});
+
+app.get("/home", (req, res) => {
+    res.render("listings/home"); 
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
